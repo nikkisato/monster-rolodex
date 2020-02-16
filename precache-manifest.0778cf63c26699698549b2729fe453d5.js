fad7900f374f21daabb8e12a4834804a5b11dcb6
Nikki <nikki.satopdx@gmail.com>
2020-02-16 15:07:00 -0800

    Updates

---
+++ precache-manifest.0778cf63c26699698549b2729fe453d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9a09c731d45d6536f11d669a14f4c0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d7a596c2fb6bb593406f",
+    "url": "/monster-rolodex/static/css/main.a3f37cc5.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a596c2fb6bb593406f",
+    "url": "/monster-rolodex/static/js/main.565eddaa.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
