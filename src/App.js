0224ec1a89ccca2a9bb88ab656f0649e90bb11a7
Nikki <nikki.satopdx@gmail.com>
2020-02-16 14:01:43 -0800

    got search terms to work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then( response => response.json())
-    .then(users =>  this.setState({ monsters: users}) )
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className='App'>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
